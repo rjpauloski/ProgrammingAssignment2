62ebfc3fb50bb2b10a44edf3b1401f3ba0faf149
rjpauloski <rjpauloski@users.noreply.github.com>
2015-04-26 14:55:14 -0400

    Initial Code Framework
    Posted in the example code from the "mean" example and began
    documentation of activity.

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,33 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
+        
+        ## 1. set the value of the matrix
+        
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        
+        ## 2. get the value of the matrix
 
+        get <- function() x
+        
+        ## 3. set the value of the inverse
+        
+        setmatrix <- function(mean) m <<- mean
+        
+        ## 4. get the value of the inverse
+        
+        getmatrix <- function() m
+        
+        ## 5. turn into list
+        
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
+        
 }
 
 
@@ -12,4 +38,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        
+        m <- x$getmean()
+        if(!is.null(m)) {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- mean(data, ...)
+        x$setmean(m)
+        m        
+        
 }
